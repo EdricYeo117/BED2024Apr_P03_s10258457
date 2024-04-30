8b764fec509aa126b71972e6c71c3bd3ebd036fd
EdricYeo117 <edricyeo.jr@gmail.com>
2024-04-30 18:13:02 +0800

    Practical 3 completed

---
+++ week03/books-api-mvc/app.js
@@ -0,0 +1,24 @@
+const book = require("../models/book");
+const express = require("express");
+const bodyParser = require("body-parser");
+const booksController = require("../controllers/booksController");
+const validateBook = require("../middlewares/validateBook");
+
+//Middleware
+app.use(bodyParser.json()); // Parse incoming JSON data in request body
+app.use(bodyParser.urlencoded({ extended: true })); // For form data handling
+
+
+// Routes
+app.get("/books", booksController.getAllBooks);
+app.get("/books/:id", booksController.getBookById);
+app.post("/books", validateBook, booksController.createBook); // Add validateBook before createBook
+app.put("/books/:id", validateBook, booksController.updateBook); // Add validateBook before updateBook
+app.delete("/books/:id", booksController.deleteBook);
+
+// Define port
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
